ddfd997e28ab504cd5fe16cf1a1007a6eb81055d
Yunus Kalkan <yunus@cntech.io>
2024-01-24 14:26:58 +0000

    minor fixes

--- middlewares/basic_auth.go
+++ middlewares/basic_auth.go
@@ -1,22 +1,26 @@
 package middlewares
 
 import (
-	cntechkitgo "github.com/cntech-io/cntechkit-go"
-	"github.com/cntech-io/cntechkit-gogin/errorcodes"
-	"github.com/cntech-io/cntechkit-gogin/responses"
-	"github.com/gin-gonic/gin"
+    cntechkitgo "github.com/cntech-io/cntechkit-go"
+    "github.com/cntech-io/cntechkit-gogin/errorcodes"
+    "github.com/cntech-io/cntechkit-gogin/responses"
+    "github.com/gin-gonic/gin"
 )
 
 func BasicAuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		username, password, ok := c.Request.BasicAuth()
-		if !ok || username == "" || password == "" {
-			cntechkitgo.NewLogger(&cntechkitgo.LoggerConfig{
-				AppName: "cntechkit-gogin",
-			}).Error(string(errorcodes.ERRC17006.Default))
-			c.JSON(responses.New().BadRequest(errorcodes.ERRC17006))
-			c.Abort()
-		}
-		c.Next()
-	}
+    return func(c *gin.Context) {
+        username, password, ok := c.Request.BasicAuth()
+        if !ok || username == "" || password == "" {
+            cntechkitgo.
+            NewLogger(
+                &cntechkitgo.LoggerConfig{
+                    AppName: "cntechkit-gogin",
+                },
+            ).
+            Error(string(errorcodes.ERRC17006.Default))
+            c.JSON(responses.New().BadRequest(errorcodes.ERRC17006))
+            c.Abort()
+        }
+        c.Next()
+    }
 }
--- server.go
+++ server.go
@@ -1,77 +1,80 @@
 package cntechkitgogin
 
 import (
-	"fmt"
+    "fmt"
 
-	cntechkitgo "github.com/cntech-io/cntechkit-go"
-	"github.com/cntech-io/cntechkit-gogin/responses"
-	"github.com/gin-gonic/gin"
+    cntechkitgo "github.com/cntech-io/cntechkit-go"
+    "github.com/cntech-io/cntechkit-gogin/responses"
+    "github.com/gin-gonic/gin"
 )
 
 type Server struct {
-	config *ServerConfig
-	router *gin.Engine
+    config *ServerConfig
+    router *gin.Engine
 }
 
 var env = cntechkitgo.NewServerEnv()
 
 func NewServer(conf ServerConfig) *Server {
-	server := &Server{}
-	server.config = &conf
-	if env.DebugModeFlag {
-		gin.SetMode(gin.DebugMode)
-	} else {
-		gin.SetMode(gin.ReleaseMode)
-	}
+    server := &Server{}
+    server.config = &conf
+    if env.DebugModeFlag {
+        gin.SetMode(gin.DebugMode)
+    } else {
+        gin.SetMode(gin.ReleaseMode)
+    }
 
-	server.router = gin.New()
+    server.router = gin.New()
 
-	server.router.SetTrustedProxies(env.TrustedProxies)
+    server.router.SetTrustedProxies(env.TrustedProxies)
 
-	return server
+    return server
 }
 
 func (s *Server) AddController(c *Controller) *Server {
-	if c.path == "" {
-		panic("controller path missing")
-	}
-	if c.version == "" {
-		panic("controller version missing")
-	}
-	group := s.router.Group(fmt.Sprintf("%v%v", c.version, c.path))
-	for _, resource := range c.apis {
-		group.Handle(string(resource.method), resource.path, append(resource.middlewares, resource.handler)...)
-	}
-	return s
+    if c.path == "" {
+        panic("controller path missing")
+    }
+    if c.version == "" {
+        panic("controller version missing")
+    }
+    group := s.router.Group(fmt.Sprintf("%v%v", c.version, c.path))
+    for _, resource := range c.apis {
+        group.Handle(string(resource.method), resource.path, append(resource.middlewares, resource.handler)...)
+    }
+    return s
 }
 
 func (s *Server) AttachMiddleWare(middleware gin.HandlerFunc) *Server {
-	s.router.Use(middleware)
-	return s
+    s.router.Use(middleware)
+    return s
 }
 
 func (s *Server) AttachHealth() *Server {
-	group := s.router.Group("/health")
-	group.GET("/", func(c *gin.Context) {
-		c.JSON(responses.New().OK("Healthy"))
-	})
-	return s
+    group := s.router.Group("/health")
+    group.GET("/", func(c *gin.Context) {
+        c.JSON(responses.New().OK("Healthy"))
+    })
+    return s
 }
 
 func (s *Server) Run() {
-	if s.config.AppPort == "" {
-		s.router.Run(":8080")
-		cntechkitgo.NewLogger(&cntechkitgo.LoggerConfig{
-			AppName: "cntechkit-gogin",
-		}).Info("Starting server on port 8080")
-	} else {
-		s.router.Run(s.config.AppPort)
-		cntechkitgo.NewLogger(&cntechkitgo.LoggerConfig{
-			AppName: "cntechkit-gogin",
-		}).Info("Starting server on port " + s.config.AppPort)
-	}
+    var PORT = ":8080"
+    if s.config.AppPort != "" {
+        PORT = s.config.AppPort
+    }
+
+    s.router.Run(PORT)
+    
+    cntechkitgo.
+    NewLogger(
+        &cntechkitgo.LoggerConfig{
+            AppName: "cntechkit-gogin",
+        },
+    ).
+    Info(fmt.Sprintf("Starting server on port: %v",PORT))
 }
 
 func (s *Server) Router() *gin.Engine {
-	return s.router
+    return s.router
 }
