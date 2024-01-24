1dfb557d90fbb4deac1c1b3196734be288c09a81
yunus <yunus.kalkan@fullog.com>
2024-01-24 20:22:07 +0300

    fix

--- server.go
+++ server.go
@@ -1,80 +1,86 @@
 package cntechkitgogin
 
 import (
-    "fmt"
+	"fmt"
 
-    cntechkitgo "github.com/cntech-io/cntechkit-go"
-    "github.com/cntech-io/cntechkit-gogin/responses"
-    "github.com/gin-gonic/gin"
+	cntechkitgo "github.com/cntech-io/cntechkit-go"
+	"github.com/cntech-io/cntechkit-gogin/responses"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
+type ServerConfig struct {
+	AppPort    string
+	CorsConfig *cors.Config
+}
+
 type Server struct {
-    config *ServerConfig
-    router *gin.Engine
+	config *ServerConfig
+	router *gin.Engine
 }
 
 var env = cntechkitgo.NewServerEnv()
 
 func NewServer(conf ServerConfig) *Server {
-    server := &Server{}
-    server.config = &conf
-    if env.DebugModeFlag {
-        gin.SetMode(gin.DebugMode)
-    } else {
-        gin.SetMode(gin.ReleaseMode)
-    }
+	server := &Server{}
+	server.config = &conf
+	if env.DebugModeFlag {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
-    server.router = gin.New()
+	server.router = gin.New()
 
-    server.router.SetTrustedProxies(env.TrustedProxies)
+	server.router.SetTrustedProxies(env.TrustedProxies)
 
-    return server
+	return server
 }
 
 func (s *Server) AddController(c *Controller) *Server {
-    if c.path == "" {
-        panic("controller path missing")
-    }
-    if c.version == "" {
-        panic("controller version missing")
-    }
-    group := s.router.Group(fmt.Sprintf("%v%v", c.version, c.path))
-    for _, resource := range c.apis {
-        group.Handle(string(resource.method), resource.path, append(resource.middlewares, resource.handler)...)
-    }
-    return s
+	if c.path == "" {
+		panic("controller path missing")
+	}
+	if c.version == "" {
+		panic("controller version missing")
+	}
+	group := s.router.Group(fmt.Sprintf("%v%v", c.version, c.path))
+	for _, resource := range c.apis {
+		group.Handle(string(resource.method), resource.path, append(resource.middlewares, resource.handler)...)
+	}
+	return s
 }
 
 func (s *Server) AttachMiddleWare(middleware gin.HandlerFunc) *Server {
-    s.router.Use(middleware)
-    return s
+	s.router.Use(middleware)
+	return s
 }
 
 func (s *Server) AttachHealth() *Server {
-    group := s.router.Group("/health")
-    group.GET("/", func(c *gin.Context) {
-        c.JSON(responses.New().OK("Healthy"))
-    })
-    return s
+	group := s.router.Group("/health")
+	group.GET("/", func(c *gin.Context) {
+		c.JSON(responses.New().OK("Healthy"))
+	})
+	return s
 }
 
 func (s *Server) Run() {
-    var PORT = ":8080"
-    if s.config.AppPort != "" {
-        PORT = s.config.AppPort
-    }
+	var PORT = ":8080"
+	if s.config.AppPort != "" {
+		PORT = s.config.AppPort
+	}
 
-    s.router.Run(PORT)
-    
-    cntechkitgo.
-    NewLogger(
-        &cntechkitgo.LoggerConfig{
-            AppName: "cntechkit-gogin",
-        },
-    ).
-    Info(fmt.Sprintf("Starting server on port: %v",PORT))
+	s.router.Run(PORT)
+
+	cntechkitgo.
+		NewLogger(
+			&cntechkitgo.LoggerConfig{
+				AppName: "cntechkit-gogin",
+			},
+		).
+		Info(fmt.Sprintf("Starting server on port: %v", PORT))
 }
 
 func (s *Server) Router() *gin.Engine {
-    return s.router
+	return s.router
 }
--- server_config.go
+++
@@ -1,8 +0,0 @@
-package cntechkitgogin
-
-import "github.com/gin-contrib/cors"
-
-type ServerConfig struct {
-	AppPort    string
-	CorsConfig *cors.Config
-}
